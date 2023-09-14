7668c3b3273c47c67d7259076d64569df00955b6
Ranamamdoh152 <140257129+Ranamamdoh152@users.noreply.github.com>
2023-09-14 23:20:52 +0300

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
-/**
-<<<<<<< HEAD
- * _isupper - checks the existance of a lower case letter
- *
- * @c: the char of intrest
- *
- * Return: 1 if is upper 0 atherwise
- */
-
-int _isupper(int c)
-{
-	while (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-
-	return (0);
-=======
- * _isupper - uppercase letters
+/*
  * @c: character to check
  *
  * Return: 0 or 1
